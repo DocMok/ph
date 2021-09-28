bb5c2e29c092934d4a2205de3e7fec60e21085d2
Александр Арендаренко <aleksandr.arendarenko@gmail.com>
2021-09-28 09:53:14 +0300

    Add created_at to notice response

--- laravel/app/Http/Resources/NoticeResource.php
+++ laravel/app/Http/Resources/NoticeResource.php
@@ -19,6 +19,7 @@ class NoticeResource extends JsonResource
     /**
      * @OA\Schema(schema="notice.response.with.project",
      *     @OA\Property(property="id", type="integer",example=3),
+     *     @OA\Property(property="created_at", type="date-time",example="2000-01-01T00:07:18.000000Z"),
      *     @OA\Property(property="text", type="string",example="Some message text"),
      *     @OA\Property(property="from_user", type="object", ref="#/components/schemas/user.response"),
      *     @OA\Property(property="project", type="object",ref="#/components/schemas/project.response"),
@@ -28,6 +29,7 @@ class NoticeResource extends JsonResource
     {
         return [
             'id' => $this->id,
+            'created_at' => $this->created_at,
             'text' => $this->text,
             'from_user' => new UserResource($this->fromUser),
             'project' => $this->when($this->notice_type == Notice::PROJECT,
