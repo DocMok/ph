9a9cf06667b37332a93733656f14bebf3de14088
DocMok <aleksandr.arendarenko@gmail.com>
2021-09-29 15:32:25 +0300

    Add amount and currency filter to suggestions

--- laravel/app/Models/Project.php
+++ laravel/app/Models/Project.php
@@ -34,7 +34,9 @@ class Project extends Model
     public function scopeSuggestions($query, User $user)
     {
         return $query->when($user->user_type == User::INVESTOR, function ($query) use ($user) {
-            $query->whereIn('category_id', $user->typeable->categories->keyBy('id')->keys());
+            $query->whereIn('category_id', $user->typeable->categories->keyBy('id')->keys())
+            ->where('amount_remaining', '<=', $user->typeable->amount)
+            ->where('currency', $user->typeable->currency);
         })
             ->when($user->user_type == User::PROJECT_OWNER, function ($query) use ($user) {
                 $lastProject = $user->typeable->projects()->orderBy('created_at', 'desc')->first();
