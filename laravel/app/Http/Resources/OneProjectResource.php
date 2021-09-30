b459f443ebe4e49290bc042687d5b8562741c181
Александр Арендаренко <aleksandr.arendarenko@gmail.com>
2021-09-30 17:37:00 +0300

    Fixed project response structure

--- laravel/app/Http/Resources/OneProjectResource.php
+++ laravel/app/Http/Resources/OneProjectResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Auth;
 
 class OneProjectResource extends JsonResource
 {
@@ -16,7 +17,18 @@ class OneProjectResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'project' => new ProjectResource($this),
+            'project' => [
+                'id' => $this->id,
+                'name' => $this->name,
+                'description' => $this->description,
+                'currency' => $this->currency,
+                'amount_available' => $this->amount_available,
+                'amount_remaining' => $this->amount_remaining,
+                'category_id' => $this->category_id,
+                'logo' => $this->logo ? asset('storage/' . $this->logo) : null,
+                'likes_total' => $this->likes()->count(),
+                'is_liked' => $this->likes()->where('user_id', Auth::user()->id)->count() == 1,
+            ],
             'owner' => new UserResource($this->projectOwner->user),
         ];
     }
