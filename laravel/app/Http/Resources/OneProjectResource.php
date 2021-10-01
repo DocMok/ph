41144406d04008d544ac2ad654368639e48c8bf2
Александр Арендаренко <aleksandr.arendarenko@gmail.com>
2021-10-01 09:39:44 +0300

    Change logic for investor suggestions Fixed swagger docs

--- laravel/app/Http/Resources/OneProjectResource.php
+++ laravel/app/Http/Resources/OneProjectResource.php
@@ -9,7 +9,19 @@ class OneProjectResource extends JsonResource
 {
     /**
      * @OA\Schema(schema="one.project.response",
-     *          @OA\Property(property="project",type="object",allOf={@OA\Schema(ref="#/components/schemas/project.response")}),
+     *          @OA\Property(property="project",type="object",
+     *              @OA\Property(property="id",type="integer",example=1),
+     *              @OA\Property(property="name",type="string",example="Best project ever"),
+     *              @OA\Property(property="description",type="string",example="Some description"),
+     *              @OA\Property(property="currency",type="string",example="usd"),
+     *              @OA\Property(property="amount_available",type="integer",example=1000),
+     *              @OA\Property(property="amount_remaining",type="integer",example=5000),
+     *              @OA\Property(property="category_id",type="integer",example=7),
+     *              @OA\Property(property="logo",type="string",example="https://partnerhub.info/some/path/to/logo.jpg"),
+     *              @OA\Property(property="likes_total",type="integer",example=2),
+     *              @OA\Property(property="is_liked",type="boolean",example=false),
+     *
+     *          ),
      *          @OA\Property(property="owner",type="object",allOf={@OA\Schema(ref="#/components/schemas/user.response")}),
      * )
      */
--- laravel/app/Models/Project.php
+++ laravel/app/Models/Project.php
@@ -34,9 +34,7 @@ class Project extends Model
     public function scopeSuggestions($query, User $user)
     {
         return $query->when($user->user_type == User::INVESTOR, function ($query) use ($user) {
-            $query->whereIn('category_id', $user->typeable->categories->keyBy('id')->keys())
-            ->where('amount_remaining', '<=', $user->typeable->amount)
-            ->where('currency', $user->typeable->currency);
+            $query->whereIn('category_id', $user->typeable->categories->keyBy('id')->keys());
         })
             ->when($user->user_type == User::PROJECT_OWNER, function ($query) use ($user) {
                 $lastProject = $user->typeable->projects()->orderBy('created_at', 'desc')->first();
