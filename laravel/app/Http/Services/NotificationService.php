a8dff95b09ce21c91dc895cd829c7a4d9aa1ffd4
Александр Арендаренко <aleksandr.arendarenko@gmail.com>
2021-10-12 17:10:46 +0300

    Change notification to raw with vibrate

--- laravel/app/Http/Services/NotificationService.php
+++ laravel/app/Http/Services/NotificationService.php
@@ -7,6 +7,7 @@ use App\Models\NotificationToken;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Kreait\Firebase\Messaging\CloudMessage;
+use Kreait\Firebase\Messaging\RawMessageFromArray;
 
 class NotificationService
 {
@@ -29,13 +30,37 @@ class NotificationService
 
         if (count($notificationTokens) > 0) {
             $tokens = array_column($notificationTokens->toArray(), 'token');
+
+
+            $message = new RawMessageFromArray([
+                'notification' => [
+                    'title' => $title,
+                    'body' => $body,
+                ],
+                'data' => [
+                    'event' => 'new_like',
+                ],
+                'android' => [
+//                    'ttl' => '3600s',
+//                    'priority' => 'normal',
+                    'vibrate' => true,
+                    'sound' => 'default',
+                    'notification' => [
+                        'title' => '$GOOG up 1.43% on the day',
+                        'body' => '$GOOG gained 11.80 points to close at 835.67, up 1.43% on the day.',
+                        'icon' => 'stock_ticker_update',
+                        'color' => '#f45342',
+                    ],
+                ],
+            ]);
+
             $notification = CloudMessage::new()->withNotification([
                 'title' => $title,
                 'body' => $body,
             ])
                 ->withDefaultSounds()
                 ->withData(['event' => 'new_like']);
-            $report = app('firebase.messaging')->sendMulticast($notification, $tokens);
+            $report = app('firebase.messaging')->sendMulticast($message, $tokens);
 
             $badTokens = array_merge($report->unknownTokens(), $report->invalidTokens());
 
