5efa8cd52dcfcf47b00617bd369ea429012397b5
Александр Арендаренко <aleksandr.arendarenko@gmail.com>
2021-10-14 13:50:30 +0300

    Add user_id to projects API

--- laravel/app/Http/Services/NotificationService.php
+++ laravel/app/Http/Services/NotificationService.php
@@ -7,7 +7,6 @@ use App\Models\NotificationToken;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Kreait\Firebase\Messaging\CloudMessage;
-use Kreait\Firebase\Messaging\RawMessageFromArray;
 
 class NotificationService
 {
@@ -31,36 +30,13 @@ class NotificationService
         if (count($notificationTokens) > 0) {
             $tokens = array_column($notificationTokens->toArray(), 'token');
 
-
-            $message = new RawMessageFromArray([
-                'notification' => [
-                    'title' => $title,
-                    'body' => $body,
-                ],
-                'data' => [
-                    'event' => 'new_like',
-                ],
-                'android' => [
-//                    'ttl' => '3600s',
-//                    'priority' => 'normal',
-                    'vibrate' => true,
-                    'sound' => 'default',
-                    'notification' => [
-                        'title' => '$GOOG up 1.43% on the day',
-                        'body' => '$GOOG gained 11.80 points to close at 835.67, up 1.43% on the day.',
-                        'icon' => 'stock_ticker_update',
-                        'color' => '#f45342',
-                    ],
-                ],
-            ]);
-
             $notification = CloudMessage::new()->withNotification([
                 'title' => $title,
                 'body' => $body,
             ])
                 ->withDefaultSounds()
                 ->withData(['event' => 'new_like']);
-            $report = app('firebase.messaging')->sendMulticast($message, $tokens);
+            $report = app('firebase.messaging')->sendMulticast($notification, $tokens);
 
             $badTokens = array_merge($report->unknownTokens(), $report->invalidTokens());
 
