148d5534cb3929eaf0f008e48ffdbf4b5c532de6
Александр Арендаренко <aleksandr.arendarenko@gmail.com>
2021-10-27 10:15:56 +0300

    Fixed letencrypt config for new domain

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(partnerhub.info)
+domains=(lynkhub.net)
 rsa_key_size=4096
 data_path="./containers/certbot"
 email="amroisu@gmail.com" # Adding a valid address is strongly recommended
